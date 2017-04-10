4af8aa069553131d3588209cfc3e5afaea4b188f
Ashkan Vahidishams <ashkan.vahidishams@bouvet.no>
2017-04-10 14:28:15 +0200

    wrong logger name

--- service/token-auth-source.py
+++ service/token-auth-source.py
@@ -23,7 +23,7 @@ def get_token(system_token):
 if __name__ == '__main__':
 
     format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logger = logging.getLogger('orchestrator-service')
+    logger = logging.getLogger('token-auth-service')
 
     # Log to stdout
     stdout_handler = logging.StreamHandler()
